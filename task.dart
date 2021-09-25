34d8337a3ed93da1da687a6a239474b86da58b13
mustafaesamm <79550524+mustafaesamm@users.noreply.github.com>
2021-09-25 22:48:48 +0300

    Create task.dart

---
+++ task.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+
+
+
+class Color {
+
+  Color(this.value);
+
+  final int value;
+  
+
+}
+
+
+class View {
+
+  int id;
+
+  Color? color;
+
+
+  View(this.id,{this.color});
+
+  @override
+
+  String toString() {
+
+    return '$id';
+
+  }
+
+}
+
+
+
+class Text extends View{
+ 
+
+
+   Text( id ,content,{Color? color}): super (id,color:color);
+    
+  }
+
+ void main() {
+   
+    int id= Random().nextInt(10000);
+  
+   var helloText = Text(id, 'Hello');
+
+  
+   print('heloo : $helloText');
+  
+ }
+
+
+void mainss() {
+  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+
+  /*
+    Separate even numbers from the above `numbers` list.
+    List<int> evenNumbers = ...
+  */
+  List<int> evenNumbers = [];
+  for (final i in numbers) {
+    if (i.isEven) {
+      evenNumbers.add(i);
+    } 
+  }
+    
+
+  print('evenNumbers: $evenNumbers');
+}
